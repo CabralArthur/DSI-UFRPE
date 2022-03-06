873072cb1297b0b9caa0a90efdee4aad1425d241
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-03-06 19:20:49 -0300

    refactor(infinity-grid): setting some code improvements and inserting grid view

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -29,43 +29,44 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
     final _saved = <WordPair>{};
+    bool _openned = true;
     final _suggestions = <WordPair>[];
     final _biggerFont = const TextStyle(fontSize: 18.0);
 
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void>(
-        builder: (context) {
-          final tiles = _saved.map(
-            (pair) {
-              return ListTile(
-                title: Text(
-                  pair.asPascalCase,
-                  style: _biggerFont,
-                ),
-              );
-            },
-          );
-          final divided = tiles.isNotEmpty
-              ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
-              : <Widget>[];
+    void _pushSaved() {
+      Navigator.of(context).push(
+        MaterialPageRoute<void>(
+          builder: (context) {
+            final tiles = _saved.map(
+              (pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+            );
+            final divided = tiles.isNotEmpty
+                ? ListTile.divideTiles(
+                    context: context,
+                    tiles: tiles,
+                  ).toList()
+                : <Widget>[];
 
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+            return Scaffold(
+              appBar: AppBar(
+                title: const Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          },
+        ),
+      );
+    }
 
     @override
-    Widget build(BuildContext context) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
@@ -75,20 +76,44 @@ class _RandomWordsState extends State<RandomWords> {
             onPressed: _pushSaved,
             tooltip: 'Saved Suggestions',
           ),
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: () {
+              setState(() {
+                _openned = !_openned;
+              });
+            },
+          ),
         ],
       ),
       body: _buildSuggestions(),
     );
   }
-    Widget _buildSuggestions() {
-      return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd){
-            return const Divider();
-          }
 
-          final int index = i ~/ 2;
+  Widget _buildSuggestions() {
+    if (_openned) {
+      return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd){
+          return const Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_suggestions[index]);
+      },
+    );
+    } else {
+      return GridView.builder(
+        padding: const EdgeInsets.all(16.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 4),
+        itemBuilder: (context, i) {
+          final int index = i;
 
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
@@ -96,8 +121,9 @@ class _RandomWordsState extends State<RandomWords> {
 
           return _buildRow(_suggestions[index]);
         },
-      );
+    );
     }
+  }
 
     Widget _buildRow(WordPair pair) {
       final alreadySaved = _saved.contains(pair);
