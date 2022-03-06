435ed49dd20564aeff7112d763987d65b61b8155
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-03-06 17:51:31 -0300

    feat(favourite-icons): adding favourite icons and menu

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -28,19 +28,58 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+    final _saved = <WordPair>{};
     final _suggestions = <WordPair>[];
     final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
     @override
     Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Startp Name Generator'),
-        ),
-        body: _buildSuggestions()
-      );
-    }
-
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.list),
+            onPressed: _pushSaved,
+            tooltip: 'Saved Suggestions',
+          ),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
     Widget _buildSuggestions() {
       return ListView.builder(
         padding: const EdgeInsets.all(16.0),
@@ -61,11 +100,27 @@ class _RandomWordsState extends State<RandomWords> {
     }
 
     Widget _buildRow(WordPair pair) {
+      final alreadySaved = _saved.contains(pair);
+
       return ListTile(
         title: Text(
           pair.asPascalCase,
           style: _biggerFont,
         ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+          semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        },
       );
     }
 }
