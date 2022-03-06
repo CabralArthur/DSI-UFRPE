0030bcf9d48c76eebeefee72f58451b95497ffe4
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2022-03-06 19:26:40 -0300

    feat(identation-and-card): inserting identation and card

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -21,51 +21,51 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWords extends StatefulWidget {
-  const RandomWords({ Key? key }) : super(key: key);
+  const RandomWords({Key? key}) : super(key: key);
 
   @override
   _RandomWordsState createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
-    final _saved = <WordPair>{};
-    bool _openned = true;
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = <WordPair>{};
+  bool _openned = true;
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
 
-    void _pushSaved() {
-      Navigator.of(context).push(
-        MaterialPageRoute<void>(
-          builder: (context) {
-            final tiles = _saved.map(
-              (pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              },
-            );
-            final divided = tiles.isNotEmpty
-                ? ListTile.divideTiles(
-                    context: context,
-                    tiles: tiles,
-                  ).toList()
-                : <Widget>[];
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = tiles.isNotEmpty
+              ? ListTile.divideTiles(
+                  context: context,
+                  tiles: tiles,
+                ).toList()
+              : <Widget>[];
 
-            return Scaffold(
-              appBar: AppBar(
-                title: const Text('Saved Suggestions'),
-              ),
-              body: ListView(children: divided),
-            );
-          },
-        ),
-      );
-    }
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
 
-    @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -93,27 +93,13 @@ class _RandomWordsState extends State<RandomWords> {
   Widget _buildSuggestions() {
     if (_openned) {
       return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd){
-          return const Divider();
-        }
-
-        final int index = i ~/ 2;
-
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-
-        return _buildRow(_suggestions[index]);
-      },
-    );
-    } else {
-      return GridView.builder(
         padding: const EdgeInsets.all(16.0),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, mainAxisSpacing: 4),
         itemBuilder: (context, i) {
-          final int index = i;
+          if (i.isOdd) {
+            return const Divider();
+          }
+
+          final int index = i ~/ 2;
 
           if (index >= _suggestions.length) {
             _suggestions.addAll(generateWordPairs().take(10));
@@ -121,32 +107,49 @@ class _RandomWordsState extends State<RandomWords> {
 
           return _buildRow(_suggestions[index]);
         },
-    );
+      );
+    } else {
+      return GridView.builder(
+        padding: const EdgeInsets.all(16.0),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, mainAxisSpacing: 4),
+        itemBuilder: (context, i) {
+          final int index = i;
+
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+
+			return Card(
+				child: _buildRow(_suggestions[index])
+			);
+		},
+      );
     }
   }
 
-    Widget _buildRow(WordPair pair) {
-      final alreadySaved = _saved.contains(pair);
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
 
-      return ListTile(
-        title: Text(
-          pair.asPascalCase,
-          style: _biggerFont,
-        ),
-        trailing: Icon(
-          alreadySaved ? Icons.favorite : Icons.favorite_border,
-          color: alreadySaved ? Colors.red : null,
-          semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
-        ),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(pair);
-            } else {
-              _saved.add(pair);
-            }
-          });
-        },
-      );
-    }
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+        semanticLabel: alreadySaved ? 'Remove from saved' : 'Save',
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
 }
